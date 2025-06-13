21f4a3672e6ae76b2fae94de92ad97b6d2782e62
SUSHIL THAKUR <mt3028788@gmail.com>
2025-06-13 12:19:05 +0530

    add new project and DbContextfile

---
+++ BookManagement.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using BookManagement.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookManagement.Infrastructure.Persistence
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+    {
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserBook> UsersBook { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<BookGenre> BookGenres { get; set; }
+        public DbSet<Favorite> Favorites { get; set; }
+        public DbSet<RefreshToken> RefreshTokens { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<UserRole> UserRoles { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+}
